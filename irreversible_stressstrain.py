c3eac36f2cca00233a8a8c1d5f404dc86d9a33cc
Edward Nusinovich <edward.nusinovich@nist.gov>
2016-06-14 17:01:00 -0400

    Run genetic algorithm on stress model

---
+++ irreversible_stressstrain.py
@@ -0,0 +1,89 @@
+"""This will contain the mcfunc for the Irreversible stress-strain model"""
+
+import numpy as np
+import math
+import matplotlib.pyplot as plot
+from matplotlib import ticker
+
+import matplotlib
+from mpl_toolkits.mplot3d import Axes3D
+
+import scipy.optimize as sciop
+from scipy.optimize import basinhopping
+import subprocess
+
+bashCommand = "make fortran"
+import subprocess
+process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+
+import irreverisble #importing the fortran mechanics routine
+
+global exp
+exp = []                           # ***** target 
+exp = np.loadtxt('ref/HSRS/22')
+
+def error_evaluation_rms(errors):
+    
+    sum_of_squares = 0
+    
+    for error in errors:
+        sum_of_squares = sum_of_squares + error**2
+        
+    return ((sum_of_squares/len(errors))**(1./2.)) #incorporated division by n, which is the proper rms 
+
+def mcfunc(model_parameters):
+
+    # -------------- number samples, =1 in this case
+    no_samples = 1
+    T_service = 22. + 273.
+    prec_stress = 0
+    SS_stress = 750
+    
+    strain_stress, WTN = irreverisble.mechanics(prec_stress,SS_stress,T_service,model_parameters,no_samples)
+    strain_stress = np.array(np.trim_zeros(strain_stress)).reshape(-1,2)
+    
+    #----------------------------
+    cal_val = []
+    errors = []
+    
+    #traverses experimental data points
+    for iexp, data in enumerate(exp[:,0]):
+        
+        #finding nearest neighbors that surround the data points, and using them to determine the error
+        for ical, data in enumerate(strain_stress[:,0]):
+            
+            ical = ical-1 # May or may not be advantageous to keep this instead of the range attribute for mem save
+            
+            left_strainpoint = strain_stress[ical,0]
+            right_strainpoint = strain_stress[ical+1,0]
+            
+            exp_datapoint = exp[iexp,0]
+            
+            if(exp_datapoint>left_strainpoint and exp_datapoint<right_strainpoint):
+                                
+                # stores the differences between the successive approximations so we interpolate
+                left_difference = exp_datapoint-left_strainpoint
+                right_difference = right_strainpoint-exp_datapoint
+                
+                total_difference = left_difference+right_difference
+                
+                left_weight = left_difference/total_difference
+                right_weight = right_difference/total_difference
+                  
+                # interpolate stress based on strain?
+                interpolated_strain = left_weight*left_strainpoint + right_weight*right_strainpoint
+                interpolated_stress = left_weight*strain_stress[ical,1] + right_weight*strain_stress[ical+1,1]
+                    
+                stress_error = interpolated_stress - exp[iexp,1]    
+                
+                #adds value, we want to find difference between these approximated data points and the real results
+                cal_val.append([interpolated_strain,interpolated_stress])                 
+                errors.append(stress_error)
+                
+                break
+    
+   
+    error_rms = error_evaluation_rms(errors)    
+    cal_val = np.asarray(cal_val)
+    
+    return error_rms
