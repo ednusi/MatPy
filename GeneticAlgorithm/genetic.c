a992e9a43f93b13887026f9495d4900ea11943bb
Edward Nusinovich <edward.nusinovich@nist.gov>
2016-06-15 11:56:10 -0400

    Display current info about population

--- GeneticAlgorithm/genetic.c
+++ GeneticAlgorithm/genetic.c
@@ -1,6 +1,5 @@
 /* Edward Nusinovich */
-/* Implementing Genetic Algorithm for polynomial function */
-
+/* Implementing Genetic Algorithm for simple functions */
 
 #include "genetic.h"
 
@@ -8,54 +7,68 @@ int main(){
 	
 	/* SETUP AND INITIAL CONDITIONS */
 	
-	int popsize = 20; /* Members of the initial population */
+	int popsize = 20; /* Members of the initial population (if odd, change loop iterations) */
 	double *pGen = malloc(sizeof(double)*popsize); /* Leaves space for twenty doubles (160 bytes) */
 	randomgen(pGen,popsize); /* Establishes first generation */
-	int tolerance = 10; /* We know that the actual solution here is 0, so if a selection is within 10 of zero, we can stop the algorithm */ 
+	int tolerance = 100; /* We know that the actual solution here is 0, so if a selection is within 10 of zero, we can stop the algorithm */ 
+	double minfitness;
 	
+	int best; /* Will store the result that is currently the optimal value on the list */
 	
-	double *newGen = malloc(sizeof(double)*popsize); /* Will hold our new generation */
-	int counter = 0; /* Stores position where members of new generation will be saved */
+	do{ /* Continues producing new populations until a members is good enough */
 	
-	long firstselection = 0;
-	long secondselection = 0;
+		int iter;
+		int totaliters = popsize/2; /* We must have as many reproductive cycles as popsize/2 --- each time, two offspring are generated */
+	
+		double *newGen = malloc(sizeof(double)*20);
+	
+		for(iter=0;iter<totaliters;iter++){
+	
+			double *newGen = malloc(sizeof(double)*popsize); /* Will hold our new generation */
+			int counter = 0; /* Stores position where members of new generation will be saved */
 
-	/* At this point we have our generation -- pGen, and our set of fitness scores -- fitness */
-	double *fitness = fitnessscores(pGen,popsize); /* Assigns a fitness score to each population member */		
-	
-	while(counter!=popsize-1){
-		
-		int *pairofchromosomes = roulettewheel(fitness, popsize);
+			double *values = funcvals(pGen,popsize);
 
-		firstselection = pGen[pairofchromosomes[0]];
-		secondselection = pGen[pairofchromosomes[1]];
-		
-		pairofchromosomes = swapchromosomes(firstselection,secondselection);
+			/* At this point we have our generation -- pGen, and our set of fitness scores -- fitness */
+			double *fitness = fitnessscores(pGen, values, popsize); /* Assigns a fitness score to each population member */		
+			
+			/* Selects two members of our population based on roulette wheel selection */
+			int *pairofchromosomes = roulettewheel(fitness, popsize);
+
+			float firstselection = pGen[pairofchromosomes[0]];
+			float secondselection = pGen[pairofchromosomes[1]];
+			
+			
+			printtable(pGen,values,fitness,popsize);
+			printf("The first selection is %f and the second is %f\n",firstselection,secondselection); exit(0);/* DEBUG, CONFIRMS THAT WE ARE RANDOMLY SELECTING FROM INIT POP */
+			
+			/* Mutation of population */
+			printf("Before mutation:\n");
+			printall(pGen,popsize);
+			
+			mutate(pGen,0.05,popsize); /* This suggests a relatively low likelihood of mutation */
+			
+			printf("After mutation\n");
+			printall(pGen,popsize);
+			
+			/* Should continue iterating until we have a full new population */
+			
+			
+			/* .... */
+			/* .... */
+			/* .... */       
+			/* .... */
+			
+			
+			pGen = newGen;
+			
+		}
 	
-	}
+	}while(minfitness>=tolerance); /* cutoff condition */
+	
+	printf("\nThe best solution found was %f at %f.\n",minfitness, best);	/* Inform at conclusion of algorithm */
+	
 	return 0;
-}
-
-/* Swaps the last 8 bits of two chromosomes (crossing-over) */
-/* WORKS FOR INTS, NOT DOUBLES */
-int *swapchromosomes(int firstchrom, int secondchrom){
-	
-	int bitshift = randinrange(7);
-	
-	int lasteightfirst = firstchrom & (0xff>>bitshift); /* gets last eight */
-	int lasteightsecond = secondchrom & (0xff>>bitshift);
-	
-	firstchrom = firstchrom ^ lasteightfirst; /* zeroes out last eight */
-	secondchrom = secondchrom ^ lasteightsecond;
-	
-	firstchrom = firstchrom | lasteightsecond; /* adds in bits from opposite solution */
-	secondchrom = secondchrom |lasteightfirst;
-	
-	int *swappedpair = malloc(sizeof(int)*2);
-	swappedpair[0] = firstchrom;
-	swappedpair[1] = secondchrom;
-	
-	return swappedpair;
 }
 
 /* Returns an array of two distinct elements that are selected based on fitness */
@@ -64,10 +77,10 @@ int *roulettewheel(double *fitness, int nums){
 	double totalfitness = baselined(fitness,nums); 
 	double *wheel = probability(fitness,totalfitness,nums); /* stores likelihood of the selection of any element [0,1) */
 	
-	int selected = rouletteindex(wheel, ((double)rand())/RAND_MAX, nums); /* This is the index of the selected member */
+	int selected = rouletteindex(wheel, randomfloat(), nums); /* This is the index of the selected member */
 	
 	int secondselection; /* Next selection shouldn't be the same as the first */
-	while((secondselection=rouletteindex(wheel,((double)rand())/RAND_MAX, nums))==selected){printf("Collision... Trying again...\n");} 
+	while((secondselection=rouletteindex(wheel,randomfloat(), nums))==selected){printf("Collision... Trying again...\n");} 
 		
 	int *pairofchromosomes = malloc(sizeof(int)*2);
 		
@@ -116,23 +129,74 @@ double *probability(double *fitness,double totalfitness, int nums){
 	return probability;
 }
 
-/* Will return a fitness score for each person */
-double *fitnessscores(double *pGen, int nums){
-
-	double *fitness = malloc(sizeof(double)*nums); /* Will store a fitness score for each point */
-
+/* Will return a fitness score for each person (distance from max in generation) */
+double *fitnessscores(double *pGen, double *values, int nums){
+	
+	double *fitness = malloc(sizeof(double)*nums);
+	double max = maxoflist(values,nums);
+	
 	int i;
 	for(i=0;i<nums;i++){
-		fitness[i]=negsquare(pGen[i]);
+		fitness[i] = max - values[i]; 
 	}
 
 	return fitness;
 }
 
-/* (Fitness Function) Negative x^2 will be our fitness function for first GA */
-double negsquare(double x){
-	return -x*x;
+double *funcvals(double *pGen, int nums){
+
+	double *values = malloc(sizeof(double)*nums); /* Will store a fitness score for each point */
+
+	int i;
+	for(i=0;i<nums;i++){
+		values[i] = objective(pGen[i]);
+	}
+
+	return values;
+	}
+
+/* (Fitness Function) x^2 will be our fitness function for first GA */
+double objective(double x){
+	return x*x;
 }
+
+
+/* Swaps the last 8 bits of two chromosomes (crossing-over) */
+/* WORKS FOR INTS, NOT DOUBLES */
+int *swapchromosomes(int firstchrom, int secondchrom){
+	
+	int bitshift = randinrange(7);
+	
+	int lasteightfirst = firstchrom & (0xff>>bitshift); /* gets last eight */
+	int lasteightsecond = secondchrom & (0xff>>bitshift);
+	
+	firstchrom = firstchrom ^ lasteightfirst; /* zeroes out last eight */
+	secondchrom = secondchrom ^ lasteightsecond;
+	
+	firstchrom = firstchrom | lasteightsecond; /* adds in bits from opposite solution */
+	secondchrom = secondchrom |lasteightfirst;
+	
+	int *swappedpair = malloc(sizeof(int)*2);
+	swappedpair[0] = firstchrom;
+	swappedpair[1] = secondchrom;
+	
+	return swappedpair;
+}
+
+
+/* Traverses each member of the population and sometimes randomly mutates based on rate */
+void mutate(double *pGen, double rate, int nums){
+	
+	int i;
+	for(i=0;i<nums;i++){
+		
+		float willmutate = randomfloat(); /* Condition for mutation */
+		
+		if(willmutate<rate)
+			pGen[i]*=(willmutate*rand()/(RAND_MAX/2)); /* We are randomly generating another number in range */
+	}
+}
+
 
 
 
--- GeneticAlgorithm/genetic.h
+++ GeneticAlgorithm/genetic.h
@@ -4,12 +4,14 @@
 
 /* PROTOTYPES */ 
 int *roulettewheel(double *fitness, int nums);
-double *fitnessscores(double *pGen, int nums);
-double negsquare(double x);
+double *fitnessscores(double *pGen, double *values, int nums);
+double objective(double x);
 double *probability(double *fitness,double totalfitness,int nums);
 double baselined(double *fitness, int nums);
 int *swapchromosomes(int firstchrom, int secondchrom);
-
+void mutate(double *pGen, double rate, int nums);
+double *funcvals(double *pGen, int nums);
+void printtable(double *pGen, double *values, double *fitness, int nums);
 
 /* Returns largest element of a list */
 double maxoflist(double *list, int nums){
@@ -26,6 +28,16 @@ double maxoflist(double *list, int nums){
 
 }
 
+/* Displays all of the current information about the population */
+void printtable(double *pGen, double *values, double *fitness, int nums){
+	
+	int i;
+	for(i=0;i<nums;i++){		
+		printf("The number is %f, its function value is %f, and its fitness score is %f.\n",pGen[i],values[i],fitness[i]);
+	}
+	
+}
+
 /* Returns smallest element of list, to baseline */
 double minoflist(double *list, int nums){
 
@@ -40,7 +52,7 @@ double minoflist(double *list, int nums){
 	return min;
 }
 
-void randomgen(long *pGen, int nums){
+void randomgen(double *pGen, int nums){
 	srand(time(NULL));
 
 	/* Generates twenty numbers, at each spot we store a different random number */
@@ -50,6 +62,11 @@ void randomgen(long *pGen, int nums){
 	}
 }
 
+/* Returns a float from zero to one */
+float randomfloat(){
+	return ((float)rand()/RAND_MAX);
+}
+
 /* Depending on pseudorandom distribution may not be completely evenly distributed */
 int randinrange(int upperbound){	
 	srand(time(NULL));
@@ -57,10 +74,10 @@ int randinrange(int upperbound){
 }
 
 /* Prints all numbers */
-void printall(long *pNums, int nums){
+void printall(double *pNums, int nums){
 	int i;
 	for(i=0;i<nums;i++){
-		printf("%l\n",pNums[i]);
+		printf("%f\n",pNums[i]);
 	}
 	printf("------------------\n");
 }
